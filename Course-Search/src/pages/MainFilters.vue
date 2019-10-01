34bb083e4201fdd26b9901852a98eeb262a660aa
Steve Lopez <lopezsteve002@gmail.com>
2019-10-01 10:30:06 -0700

    Added the three main buttons on filters

---
+++ Course-Search/src/pages/MainFilters.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+		<div class = "div">
+			<h3>Term</h3>
+				<div align = "Left">
+				<input type = "button" class = "button" value = "Semester Term">
+				</div>
+		</div>
+		<div class = "div">
+			<h3>Sessions</h3>
+				<div align = "left">
+					<input type = "button" class = "button" value = "Session">
+				</div>
+		</div>
+		<div class = "div">
+			<h3>Course Status</h3>
+				<div align = "left">
+					<input type = "button" class = "button" value = "Only Open">
+				</div>
+		</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
