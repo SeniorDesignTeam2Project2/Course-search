4d93629270ed69afa9b79da50efbf13fc2812f1c
Steve Lopez <lopezsteve002@gmail.com>
2019-10-08 17:36:04 -0700

    Changed button color and shape

--- Course-Search/src/components/MainFilters.vue
+++ Course-Search/src/components/MainFilters.vue
@@ -1,32 +1,52 @@
 <template>
     <div>
-		<div class = "div">
-			<h3>Term</h3>
-				<div align = "Left">
-				<input type = "button" class = "button" value = "Semester Term">
+		<div>
+			<h3 class = "work">Term</h3>
+			
+				<div class = "divmain" align = "Right">
+				
+						<button v-on:click = "terms" class = "button"> Change </button>
 				</div>
 		</div>
+
 		<div class = "div">
-			<h3>Sessions</h3>
-				<div align = "left">
-					<input type = "button" class = "button" value = "Session">
+			<h3 class = "work">Sessions</h3>
+				<div class = "divmain" align = "Right">
+					<input type = "button" class = "button" value = "Change">
 				</div>
 		</div>
+
 		<div class = "div">
-			<h3>Course Status</h3>
-				<div align = "left">
-					<input type = "button" class = "button" value = "Only Open">
+			<h3 class = "work">Course Status</h3>
+				<div class = "divmain" align = "Right">
+					<input type = "button" class = "button" value = "Change">
 				</div>
 		</div>
+
+
 	</div>
 </template>
 
 <script>
 export default {
-    name: 'MainFilters',
+	name: 'MainFilters',
+	
 }
+
+
 </script>
 
 <style scoped>
+.button {
+	background-color: white;
+	border-radius: 4px;
+	border: 1.5px solid #DCDCDC;
+	font-size: 12px;
+	padding: 10px 20px;
+	text-align: center;
+	font-weight: bold;
+
+}
+
 
 </style>
